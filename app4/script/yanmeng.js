a012437a4066962131587c74d59518b4e788b327
yanmeng1 <ymeng125@126.com>
2016-09-18 00:52:00 +0800

    unfix

---
+++ app4/script/yanmeng.js
@@ -0,0 +1,27 @@
+function fnTime () {
+
+    var myTime = new Date();
+    
+    // number
+    var iYear = myTime.getFullYear();
+    var iMonth = myTime.getMonth()+1;
+    var iDate = myTime.getDate();
+    var iWeek = myTime.getDay();
+    var iHours = myTime.getHours();
+    var iMin = myTime.getMinutes();
+    var iSec = myTime.getSeconds();
+    var str = '';
+    
+    if( iWeek === 0 ) iWeek = '星期日';
+    if( iWeek === 1 ) iWeek = '星期一';
+    if( iWeek === 2 ) iWeek = '星期二';
+    if( iWeek === 3 ) iWeek = '星期三';
+    if( iWeek === 4 ) iWeek = '星期四';
+    if( iWeek === 5 ) iWeek = '星期五';
+    if( iWeek === 6 ) iWeek = '星期六';
+    
+    str = iYear+ '-' +iMonth+'-'+iDate;
+    
+    return str;
+
+}
