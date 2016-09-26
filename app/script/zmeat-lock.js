d81b98889174c428456e504bae936e5302038709
zmeat <zmeatzhou@126.com>
2016-09-27 00:40:18 +0800

    start my hand

---
+++ app/script/zmeat-lock.js
@@ -0,0 +1,10 @@
+/**
+ * zmeat-lock.js
+ * @author zmeat
+ * @params window
+ * 
+ * 此模块是对apicloud 锁屏模块的封装，以根据不同的移动端做出不同响应，即重载的思想
+ * needs : screenLock,patternLock 两个模块
+ */
+
+ #TODO
---
+++ app/script/zmeat-uicalender.js
@@ -0,0 +1,9 @@
+/**
+ * zmeat-uicalender.js
+ * @author zmeat
+ * @params window
+ * 
+ * 此模块是对apicloud uicalender的一个再次封装以满足自身的需求
+ */
+
+ #TODO
