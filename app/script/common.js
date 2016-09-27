17a47846eb1dc2815d62360c7145410788253ae7
zmeat <zmeatzhou@126.com>
2016-09-28 01:19:43 +0800

    some change

--- app/script/common.js
+++ app/script/common.js
@@ -172,3 +172,64 @@ function openSelector(title, items, singleSelection, cb){
 	    }
 	 });
 }
+
+//h5申请全屏方法简易封装
+function fullScreen(element) {
+	this.element = element;
+	this.in = function(){
+		if(typeof this.element == 'undefined'){
+			return alert('element is needed');
+		}
+	  	if(this.element.requestFullScreen) {
+	    	this.element.requestFullScreen();
+	  	} else if(this.element.webkitRequestFullScreen ) {
+	    	element.webkitRequestFullScreen();
+	  	} else if(this.element.mozRequestFullScreen) {
+	    	this.element.mozRequestFullScreen();
+	  	}
+	}
+	this.out = function(){
+		if (document.exitFullscreen) {
+	        document.exitFullscreen();
+	    }
+	    else if (document.webkitCancelFullScreen) {
+	        document.webkitCancelFullScreen();
+	    }
+	    else if (document.mozCancelFullScreen) {
+	        document.mozCancelFullScreen();
+	    }else{
+	    	document.cancelFullScreen();
+	    }
+	}
+}
+
+
+//时间格式化 引自网络
+
+// 对Date的扩展，将 Date 转化为指定格式的String
+// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符， 
+// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 
+// 例子： 
+// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423 
+// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18 
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+/*
+调用： 
+var time1 = new Date().Format("yyyy-MM-dd");
+var time2 = new Date().Format("yyyy-MM-dd HH:mm:ss");  
+*/
+
